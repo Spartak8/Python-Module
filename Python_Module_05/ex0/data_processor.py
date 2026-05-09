f8b826dd5a9ef2509940953e83919c8962c2eea0
Spartak <77225767+Spartak8@users.noreply.github.com>
2026-05-09 11:39:21 +0400

    Add files via upload

--- Python_Module_05/ex0/data_processor.py
+++ Python_Module_05/ex0/data_processor.py
@@ -1,43 +1,80 @@
 from abc import ABC, abstractmethod
 from typing import Any
 
+
 class DataProcessor(ABC):
+    def __init__(self):
+        self._storage = []
+        self._total = 0
+
     @abstractmethod
     def validate(self, data: Any) -> bool:
         pass
-    
 
     @abstractmethod
     def ingest(self, data: Any) -> None:
         pass
-    
 
     def output(self) -> tuple[int, str]:
-        pass
-    
+        rank = self._total - len(self._storage)
+        first = self._storage.pop(0)
+        return (rank, first)
+
 
 class NumericProcessor(DataProcessor):
     def validate(self, data: Any) -> bool:
-        pass
-    
+        if isinstance(data, list):
+            return all(isinstance(x, (int, float)) and not isinstance(x, bool)
+                       for x in data)
+        return isinstance(data, (int, float)) and not isinstance(data, bool)
 
     def ingest(self, data: Any) -> None:
-        pass
-    
+        if not self.validate(data):
+            raise ValueError("Improper numeric data")
+        if isinstance(data, list):
+            for x in data:
+                self._storage.append(str(x))
+                self._total += 1
+        else:
+            self._storage.append(str(data))
+            self._total += 1
+
 
 class TextProcessor(DataProcessor):
     def validate(self, data: Any) -> bool:
-        pass
-    
+        if isinstance(data, list):
+            return all(isinstance(x, str) for x in data)
+        return isinstance(data, str)
 
     def ingest(self, data: Any) -> None:
-        pass
-    
+        if not self.validate(data):
+            raise ValueError("Improper text data")
+        if isinstance(data, list):
+            for x in data:
+                self._storage.append(x)
+                self._total += 1
+        else:
+            self._storage.append(data)
+            self._total += 1
+
 
 class LogProcessor(DataProcessor):
     def validate(self, data: Any) -> bool:
-        pass
+        if isinstance(data, dict):
+            keys_ok = all(isinstance(k, str) for k in data.keys())
+            vals_ok = all(isinstance(v, str) for v in data.values())
+            return keys_ok and vals_ok
+        elif isinstance(data, list):
+            return all(self.validate(k) for k in data)
+        return False
     
-
     def ingest(self, data: Any) -> None:
-        pass
+        if not self.validate(data):
+            raise ValueError("Improper log data")
+        if isinstance(data, list):
+            for x in data:
+                self._storage.append(f"{x['log_level']}: {x['log_message']}")
+                self._total += 1
+        else:
+            self._storage.append(f"{data['log_level']}: {data['log_message']}")
+            self._total += 1
